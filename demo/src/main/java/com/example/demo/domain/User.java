c57423ccfa45584f0d9567586857ed60098273d9
niepu <niepu1991@163.com>
2019-01-10 23:10:21 +0800

    maven插件逆向生成mapper和domain文件

---
+++ demo/src/main/java/com/example/demo/domain/User.java
@@ -0,0 +1,55 @@
+package com.example.demo.domain;
+
+import java.util.Date;
+
+public class User {
+    private Long id;
+
+    private String username;
+
+    private String phone;
+
+    private String address;
+
+    private Date birth;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username == null ? null : username.trim();
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone == null ? null : phone.trim();
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+
+    public Date getBirth() {
+        return birth;
+    }
+
+    public void setBirth(Date birth) {
+        this.birth = birth;
+    }
+}
