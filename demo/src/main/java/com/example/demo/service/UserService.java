5c114e8f2deeb40343e2da91526ffad57c45414a
niepu <niepu1991@163.com>
2019-01-12 15:22:15 +0800

    事务方法中添加sleep陷入死循环诡异问题

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -4,6 +4,7 @@ import com.example.demo.domain.User;
 import com.example.demo.domain.UserExample;
 import com.example.demo.mapper.UserMapper;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,7 +23,9 @@ public class UserService {
     @Resource
     private UserMapper userMapper;
 
-    @Transactional
+    @Autowired
+    private UserService userService;
+
     public User selectByPrimaryKey(long id) {
         return userMapper.selectByPrimaryKey(id);
     }
@@ -64,6 +67,20 @@ public class UserService {
 
     @Transactional
     public int updateByPrimaryKeySelective(User user){
-        return userMapper.updateByPrimaryKeySelective(user);
+        User u = userService.selectByPrimaryKey(user.getId());
+        if (u.getUsername().equals("postman")) {
+            // 休息一分钟，先获取满足修改条件，然后让其他事务修改，让条件不满足
+            // 看看这个事务是否还能否执行成功
+            // 在这里真是见鬼了，陷入死循环了，不知道是个什么诡异的问题
+            System.out.println("进入条件");
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("u.getUsername===="+u.getUsername());
+            return userService.updateByPrimaryKeySelective(user);
+        }
+        return -1;
     }
 }
