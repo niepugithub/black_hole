3f659664c2ae37402ecb3a3d0c9ca7d6b985d822
niepu <niepu1991@163.com>
2019-01-12 17:30:24 +0800

    UserController改名

--- demo/src/main/java/com/example/demo/service/UserService.java
+++ demo/src/main/java/com/example/demo/service/UserService.java
@@ -39,22 +39,24 @@ public class UserService {
     public List<User> queryUsers(User user) {
         UserExample example = new UserExample();
         UserExample.Criteria criteria = example.createCriteria();
-        // 暂时没找到好的方法，只能死拼凑查询条件
-        if (StringUtils.isNotBlank(user.getAddress())) {
-            // 注意模糊查询的百分号问题
-            criteria.andAddressLike("%" + user.getAddress() + "%");
-        }
-        if (null != (user.getBirth())) {
-            criteria.andBirthEqualTo(user.getBirth());
-        }
-        if (null != user.getId()) {
-            criteria.andIdEqualTo(user.getId());
-        }
-        if (StringUtils.isNotBlank(user.getPhone())) {
-            criteria.andPhoneLike("%" + user.getPhone() + "%");
-        }
-        if (StringUtils.isNotBlank(user.getUsername())) {
-            criteria.andUsernameLike("%" + user.getUsername() + "%");
+        if(user!=null){
+            // 暂时没找到好的方法，只能死拼凑查询条件
+            if (StringUtils.isNotBlank(user.getAddress())) {
+                // 注意模糊查询的百分号问题
+                criteria.andAddressLike("%" + user.getAddress() + "%");
+            }
+            if (null != (user.getBirth())) {
+                criteria.andBirthEqualTo(user.getBirth());
+            }
+            if (null != user.getId()) {
+                criteria.andIdEqualTo(user.getId());
+            }
+            if (StringUtils.isNotBlank(user.getPhone())) {
+                criteria.andPhoneLike("%" + user.getPhone() + "%");
+            }
+            if (StringUtils.isNotBlank(user.getUsername())) {
+                criteria.andUsernameLike("%" + user.getUsername() + "%");
+            }
         }
         List<User> users = userMapper.selectByExample(example);
         return users;
